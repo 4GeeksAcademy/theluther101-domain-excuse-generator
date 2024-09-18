ae0cd11143c4ef7a733001fea84c37f88ce2e427
Theluther101 <afolabi12356@gmail.com>
2024-09-18 02:43:20 +0000

    pro done

--- src/app.js
+++ src/app.js
@@ -5,7 +5,35 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  // write you code here
+  document.querySelector("#btn").addEventListener("click", () => {
+    const domains = generateDomain();
+    displayDomains(domains);
+  });
+
+  let generateDomain = () => {
+    let pronoun = ["the", "our"];
+    let adj = ["great", "big"];
+    let noun = ["jogger", "racoon"];
+    var list = [];
+
+    for (let i = 0; i < pronoun.length; i++) {
+      for (let j = 0; j < adj.length; j++) {
+        for (let k = 0; k < noun.length; k++) {
+          list.push(pronoun[i] + adj[j] + noun[k] + ".com");
+        }
+      }
+    }
+    return list;
+  };
+  let displayDomains = domains => {
+    const domainList = document.querySelector("#the-domain");
+    domainList.innerHTML = "";
+    domains.forEach(domain => {
+      const listItem = document.createElement("li");
+      listItem.textContent = domain;
+      domainList.appendChild(listItem);
+    });
+  };
 };
